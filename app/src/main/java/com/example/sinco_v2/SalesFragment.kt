0da21918690640724ea0f17b270f073ac433119f
lewis <lewispunzalan21@gmail.com>
2023-12-08 22:22:38 +0800

    12/07/23 update

--- app/src/main/java/com/example/sinco_v2/SalesFragment.kt
+++ app/src/main/java/com/example/sinco_v2/SalesFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+
 class SalesFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,7 +19,44 @@ class SalesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sales, container, false)
+        val view = inflater.inflate(R.layout.fragment_sales, container, false)
+
+        // Add your fragments for each tab
+        val allSalesFragment = AllSalesFragment()// AllSalesFragment()
+        val consignorsFragment = ConsignorSalesFragment()//ConsignorsFragment()
+
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, allSalesFragment)
+            .commit()
+
+        val tabLayout = view.findViewById<TabLayout>(R.id.tabLayout)
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                // Handle tab selection
+                when (tab?.position) {
+                    0 -> {
+                        parentFragmentManager.beginTransaction()
+                            .replace(R.id.fragmentContainer, allSalesFragment)
+                            .commit()
+                    }
+                    1 -> {
+                        parentFragmentManager.beginTransaction()
+                            .replace(R.id.fragmentContainer, consignorsFragment)
+                            .commit()
+                    }
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                // Handle tab unselection
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                // Handle tab reselection
+            }
+        })
+
+        return view
     }
 
 }
